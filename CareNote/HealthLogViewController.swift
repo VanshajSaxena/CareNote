833d402abb359e9dceaeaf2c1a396c8bdf95e6ab
Anay Dubey <edu.anay.dubey@gmail.com>
2024-05-20 16:38:37 +0530

    healthLog
    segmented control & filter functionality

--- CareNote/HealthLogViewController.swift
+++ CareNote/HealthLogViewController.swift
@@ -16,12 +16,33 @@ class HealthLogViewController: UIViewController {
         super.tabBarItem.selectedImage = UIImage(systemName: "calendar.circle.fill")
     }
 
+    @IBOutlet var filter: UIDatePicker!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        filter.datePickerMode = .
+        filter.addTarget(self, action: #selector(filter(_:)), for: .valueChanged)
 
         // Do any additional setup after loading the view.
     }
     
+    @objc func filter(_ sender: UIDatePicker) {
+        let selectedDate = sender.date
+        
+        let calendar = Calendar.current
+        let month = calendar.component(.month, from: selectedDate)
+        let year = calendar.component(.year, from: selectedDate)
+        
+        
+//        filteredEntries = timelineEntries.filter { entry in
+//            let entryMonth = calendar.component(.month, from: entry.date)
+//            let yearMonth = calendar.component(.year, from: entry.date)
+//            return entryMonth == month && entryYear == year
+//        }
+        
+    }
 
     /*
     // MARK: - Navigation
