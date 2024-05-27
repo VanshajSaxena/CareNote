96846ff539162a093778335faba334482ac8c5cb
Yash Maurya <yashmaurya7812@gmail.com>
2024-05-27 16:50:32 +0530

    Added Scroll View

--- CareNote/HealthViewViewController.swift
+++ CareNote/HealthViewViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class HealthViewViewController: UIViewController {
+class HealthViewViewController: UIViewController,UITableViewDataSource, UITabBarDelegate, UITableViewDelegate {
     required init?(coder: NSCoder)
     {
         super.init(coder: coder)
@@ -17,6 +17,7 @@ class HealthViewViewController: UIViewController {
     }
 
     @IBOutlet var currentVitalsBackView: UIView!
+    @IBOutlet var recentReportBackView: UIView!
     @IBOutlet var currentVitalsLabel: UILabel!
     @IBOutlet var currentVitalsButton: UIButton!
     @IBOutlet var bpView: UIView!
@@ -24,11 +25,16 @@ class HealthViewViewController: UIViewController {
     @IBOutlet var bloodSugarView: UIView!
     @IBOutlet var hrView: UIView!
     @IBOutlet var creatinineView: UIView!
+    @IBOutlet var recentReportTableView: UITableView!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //dummy table
+        recentReportTableView.dataSource = self
+        recentReportTableView.delegate = self
         
         updateUI()
     }
@@ -47,6 +53,73 @@ class HealthViewViewController: UIViewController {
         hrView.layer.cornerRadius = 10
         creatinineView.layer.cornerRadius = 10
         
-    //
+    //Recent Report UI
+        //Recent Report Back View
+        recentReportBackView.backgroundColor = UIColor.white.withAlphaComponent(0.1)
+        recentReportBackView.layer.cornerRadius = 10
+        
+        //Table View
+        recentReportTableView.layer.cornerRadius = 10
+        
+    }
+    
+    //Dummy Data
+    struct BloodTest {
+        let parameter: String
+        let value: String
+        let unit: String
+        let isAbnormal: Bool
+    }
+    
+    var bloodTests: [BloodTest] = [
+        BloodTest(parameter: "Haemoglobin", value: "8.70", unit: "g/dL", isAbnormal: true),
+        BloodTest(parameter: "Packed Cell Volume (HCT)", value: "27.6", unit: "%", isAbnormal: true),
+        BloodTest(parameter: "R.B.C. Count", value: "4.34", unit: "mill/μL", isAbnormal: false),
+        BloodTest(parameter: "W.B.C. Count", value: "4600", unit: "cell/μL", isAbnormal: false),
+        BloodTest(parameter: "Packed Cell Volume (HCT)", value: "27.6", unit: "%", isAbnormal: true),
+        BloodTest(parameter: "R.B.C. Count", value: "4.34", unit: "mill/μL", isAbnormal: false),
+        BloodTest(parameter: "W.B.C. Count", value: "4600", unit: "cell/μL", isAbnormal: false),
+        BloodTest(parameter: "Packed Cell Volume (HCT)", value: "27.6", unit: "%", isAbnormal: true),
+        BloodTest(parameter: "R.B.C. Count", value: "4.34", unit: "mill/μL", isAbnormal: false),
+        BloodTest(parameter: "W.B.C. Count", value: "4600", unit: "cell/μL", isAbnormal: false),
+        BloodTest(parameter: "Packed Cell Volume (HCT)", value: "27.6", unit: "%", isAbnormal: true),
+        BloodTest(parameter: "R.B.C. Count", value: "4.34", unit: "mill/μL", isAbnormal: false),
+        BloodTest(parameter: "W.B.C. Count", value: "4600", unit: "cell/μL", isAbnormal: false),
+        BloodTest(parameter: "Packed Cell Volume (HCT)", value: "27.6", unit: "%", isAbnormal: true),
+        BloodTest(parameter: "R.B.C. Count", value: "4.34", unit: "mill/μL", isAbnormal: false),
+        BloodTest(parameter: "W.B.C. Count", value: "4600", unit: "cell/μL", isAbnormal: false)
+    ]
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+
+    func tableView( _ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return bloodTests.count
+        }
+
+    func tableView( _ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "BloodTestCell", for: indexPath)
+
+            let parameterLabel = cell.viewWithTag(1) as! UILabel
+            let valueLabel = cell.viewWithTag(2) as! UILabel
+            let unitLabel = cell.viewWithTag(3) as! UILabel
+
+            let bloodTest = bloodTests[indexPath.row]
+
+            parameterLabel.text = bloodTest.parameter
+            valueLabel.text = bloodTest.value
+            unitLabel.text = bloodTest.unit
+
+            // Highlight abnormal values
+            if bloodTest.isAbnormal {
+                valueLabel.textColor = .red
+            } else {
+                valueLabel.textColor = .green
+            }
+
+        return cell
     }
 }
