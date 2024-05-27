8dea22deab41a82032c143a5c6b5a695115026a9
Sameer Verma <happyverma805@gmail.com>
2024-05-27 10:33:19 +0530

    Date Picker Visible Action Added

--- CareNote/MedMinderTableViewController.swift
+++ CareNote/MedMinderTableViewController.swift
@@ -16,17 +16,80 @@ class MedMinderTableViewController: UITableViewController {
         super.tabBarItem.selectedImage = UIImage(systemName: "pills.fill")
     }
     
+    //UI Outlet for the date filter of the application
+    
+    @IBOutlet var FilterDateLabel: UILabel!
+    @IBOutlet var FilterDatePicker: UIDatePicker!
+//---------------------------------------------------------------
+    
+    // Here we are saving the index path of the date label and date Picker Value
+    var filterDateLabelIndexPath = IndexPath(row: 0, section: 0)
+    var FilterDatePickerIndexPath = IndexPath(row: 1, section: 0)
 
+    // this is the Variable used for the checking that is the Date Picker is visible or not
+    var isFilterDatePickerVisible:Bool = false{
+        didSet{
+            FilterDatePicker.isHidden = !isFilterDatePickerVisible
+        }
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath{
+//
+        case FilterDatePickerIndexPath:
+                    return isFilterDatePickerVisible ? 217 : 0
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath == filterDateLabelIndexPath {
+                    isFilterDatePickerVisible.toggle()
+                } else {
+//                    // If any other row is selected, we hide the date picker if it's visible
+//                    if isFilterDatePickerVisible {
+//                        isFilterDatePickerVisible = false}
+                    return
+                    }
+                
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+    
+// ----------  VIEW DID LOAD ------------------------
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+//        let midnightToday = Calendar.current.startOfDay(for: Date())
+//        FilterDatePicker.minimumDate = midnightToday
+        updateDateview() // calling the function when application just open
+        
     }
-
+    
+    // this is the function which is used to update the Filter Date Label according to the Date Picker
+    
+// **************************************************************************************
+// ------------------ DATE PICKER ------------------------------
+    func updateDateview(){
+        FilterDateLabel.text = FilterDatePicker.date.formatted(date: .abbreviated, time: .omitted)
+        
+    }
+    
+    
+    
+    // Filter date picker Action Outlet -- filterDateChanged -- Whenever Date changed
+    
+    @IBAction func filterDateChanged(_ sender: UIDatePicker) {
+        updateDateview()
+        print("date \(FilterDatePicker.date.formatted(date: .abbreviated, time: .omitted))")
+    }
+    
+// **************************************************************************************
+    
     // MARK: - Table view data source
 
 //    override func numberOfSections(in tableView: UITableView) -> Int {
