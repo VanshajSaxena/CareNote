99c169eeeb0b7c06971d2fbdcde056db1a0dfda2
Yash Maurya <yashmaurya7812@gmail.com>
2024-05-29 15:13:40 +0530

    New Collection view storyboard

---
+++ CareNote/HealthView/HealthViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HealthViewCell.swift
+//  CareNote
+//
+//  Created by Batch-1 on 29/05/24.
+//
+
+import Foundation
+
+import UIKit
+
+class HealthViewCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
